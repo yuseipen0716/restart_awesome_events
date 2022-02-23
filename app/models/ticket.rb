cb81b9e18e420ebef88594dc1d6b436b209885cf
yuseipen0716 <yuseipen1@gmail.com>
2022-02-24 00:28:44 +0900

    add validation to event

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,6 @@
 class Ticket < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true # optional: trueを記述すると、外部キーのnilを許容
   belongs_to :event
+
+  validates :comment, length: { maximum: 30 }, allow_blank: true # commentは30文字以内、空文字やnilを許可
 end
