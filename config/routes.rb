6760ab45fd174dcaeaeae387fcb14e921a805661
yuseipen0716 <yuseipen1@gmail.com>
2022-02-25 14:09:32 +0900

    implement retirements and before destroy user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get "/auth/:provider/callback" => "sessions#create"
+  delete "/logout" => "sessions#destroy", as: :logout
+  resource :retirements
   resources :events do
     resources :tickets
   end
-  root 'welcome#index'
-  get "/auth/:provider/callback" => "sessions#create"
-  delete "/logout" => "sessions#destroy"
 end
