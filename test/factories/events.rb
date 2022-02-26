1e9366f63bd35bd9263f20ff4f9cc56d4be7acf9
yuseipen0716 <yuseipen1@gmail.com>
2022-02-27 03:42:00 +0900

    fix a little

--- test/factories/events.rb
+++ test/factories/events.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     sequence(:name){ |i| "イベント名#{i}" }
     sequence(:place){ |i| "イベント開催場所#{i}" }
     sequence(:content){ |i| "イベント本文#{i}" }
-    start_at { rand(1..30).days.from.now }
+    start_at { rand(1..30).days.from_now }
     end_at { start_at + rand(1..30).hours }
   end
 end
