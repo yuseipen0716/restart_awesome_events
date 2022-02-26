6ee28f9cff961beff9e726d5c84d6fc9278a122a
yuseipen0716 <yuseipen1@gmail.com>
2022-02-27 07:33:45 +0900

    controller test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy", as: :logout
+  get 'status' => 'status#index', defaults: { format: 'json'}
   resource :retirements
   resources :events do
     resources :tickets
