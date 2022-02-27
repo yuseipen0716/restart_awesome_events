bebbd901bb96555cd95ac21b29fdda80045dd683
yuseipen0716 <yuseipen1@gmail.com>
2022-02-27 14:21:42 +0900

    test practice using mock

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,15 +3,16 @@ require "test_helper"
 class EventTest < ActiveSupport::TestCase
   test "#created_by? owner_idと引数の#idが同じ時" do
     event = FactoryBot.create(:event)
-    user = User.new
-    user.stub(:id, event.owner_id) do
-      assert_equal(true, event.created_by?(user))
-    end
+    user = Minitest::Mock.new.expect(:id, event.owner_id)
+    assert_equal(true, event.created_by?(user))
+    user.verify
   end
   test "#created_by? owner_idと引数の#idが異なる時" do
     event = FactoryBot.create(:event)
+    user = Minitest::Mock.new.expect(:id, event.owner_id)
     another_user = FactoryBot.create(:user)
     assert_equal(false, event.created_by?(another_user))
+    user.verify # メソッドが呼び出されていない場合はMockExpectationErrorがでる
   end
   test "#created_by? 引数がnilな時" do
     event = FactoryBot.create(:event)
