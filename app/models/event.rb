040abcc1d04f34f5cff9c464ce3553b1ea437b63
yuseipen0716 <yuseipen1@gmail.com>
2022-02-28 16:55:07 +0900

    upload image

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ApplicationRecord
     has_one_attached :image
     has_many :tickets, dependent: :destroy
     belongs_to :owner, class_name: "User"
+    attr_accessor :remove_image
+    before_save :remove_image_if_user_accept
     validates :name, length: { maximum: 50 }, presence: true
     validates :place, length: { maximum: 100 }, presence: true
     validates :content, length: { maximum: 2000 }, presence: true
@@ -23,4 +25,9 @@ class Event < ApplicationRecord
             errors.add(:start_at, "は終了時間よりも前に設定してください")
         end
     end
+
+    def remove_image_if_user_accept
+        self.image = nil if ActiveRecord::Type::Boolean.new.cast(remove_image)
+    end
+
 end
