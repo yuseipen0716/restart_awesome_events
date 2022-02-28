00b373dc2931eadf699b6420b54966b2908ae7be
yuseipen0716 <yuseipen1@gmail.com>
2022-02-28 11:04:10 +0900

    event test

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -9,13 +9,29 @@ class EventTest < ActiveSupport::TestCase
   end
   test "#created_by? owner_idと引数の#idが異なる時" do
     event = FactoryBot.create(:event)
-    user = Minitest::Mock.new.expect(:id, event.owner_id)
+    # user = Minitest::Mock.new.expect(:id, event.owner_id)
     another_user = FactoryBot.create(:user)
     assert_equal(false, event.created_by?(another_user))
-    user.verify # メソッドが呼び出されていない場合はMockExpectationErrorがでる
+    # user.verify # メソッドが呼び出されていない場合はMockExpectationErrorがでる
   end
   test "#created_by? 引数がnilな時" do
     event = FactoryBot.create(:event)
     assert_equal(false, event.created_by?(nil))
+  end
+
+  test "start_at_should_be_before_end_at validation OK" do
+    start_at = rand(1..30).days.from_now
+    end_at =start_at + rand(1..30).hours
+    event = FactoryBot.build(:event, start_at: start_at, end_at: end_at)
+    event.valid?
+    assert_empty(event.errors[:start_at])
+  end
+
+  test "start_at_should_be_before_end_at validation error" do
+    start_at = rand(1..30).days.from_now
+    end_at =start_at - rand(1..30).hours
+    event = FactoryBot.build(:event, start_at: start_at, end_at: end_at)
+    event.valid?
+    assert_not_empty(event.errors[:start_at])
   end
 end
